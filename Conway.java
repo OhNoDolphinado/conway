84c25c6562b6a9857f805161de5d9d3d02f40de8
apcsa <apcsa@ausdk12.org>
2023-05-17 15:05:07 -0700

    save

--- Conway.java
+++ Conway.java
@@ -11,12 +11,12 @@
 
 public class Conway{
     private int boardSize;
-    private int timesRun;
+    private int timesToRun;
     private int[][] board;
 
-    public Conway(int boardSize, int timesRun){
+    public Conway(int boardSize, int timesToRun){
         this.boardSize = boardSize;
-        this.timesRun = timesRun;
+        this.timesToRun = timesToRun;
         board = new int[boardSize][boardSize];
     }
 
@@ -37,16 +37,55 @@ public class Conway{
             String temp = "";
             for(int col = 0; col < board[0].length; col++){    
                 if (board[row][col] == 1){ // alive
-                    temp = temp + "⬜";
+                    temp = temp + "# ";
                 } else {
-                    temp = temp + "⬛"; // dead
+                    temp = temp + "O "; // dead
                 }
             }
             System.out.println(temp);
         }
     }
 
-    public void print(int runs){
-        
+    public int neighbourChecker(int row, int col){ // int and col of the selected cell
+        int aliveSum = 0;
+        for(int r = row - 1; r <= row + 1
+        && row > 0
+        && row < board.length - 1
+        ; r++){
+            for(int c = col - 1; c <= col + 1
+            && col > 0
+            && col < board[0].length - 1
+            ; c++){
+                aliveSum += board[r][c];
+            }
+        }
+        return aliveSum;
+        // TODO: edge cases (ie the sides of the board, top of the board, etc)
+    }
+
+    public void run(){
+        for (int runs = timesToRun; runs > 0; runs--) { // counts down the times we run this
+            int[][] tempBoard = new int[boardSize][boardSize]; // creates dummy board
+
+            for(int row = 0; row < board.length; row++){ // iterate through 2D board
+                for(int col = 0; col < board[0].length; col++){
+                    // 1. Any live cell with two or three live neighbours survives.
+                    if (board[row][col] == 1 && (neighbourChecker(row, col) == 2 || neighbourChecker(row, col) == 3)) { 
+                        tempBoard[row][col] = 1;
+                    } 
+                    // 2. Any dead cell with three live neighbours becomes a live cell.
+                    else if (board[row][col] == 0 && neighbourChecker(row, col) == 3){ 
+                        tempBoard[row][col] = 1;
+                    } 
+                    // 3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+                    else { 
+                        tempBoard[row][col] = 0;
+                    }
+                }
+            }
+
+            board = tempBoard; // sets the real board to the dummy board
+            printBoard();
+        }
     }
 }
--- ConwayMain.java
+++ ConwayMain.java
@@ -2,7 +2,17 @@ public class ConwayMain {
     public static void main(String[] args) {
         Conway c = new Conway(10, 10);
         c.startBoard();
-        c.seed(2, 4);
-        c.printBoard();
+        c.seed(0, 0);
+        c.seed(2, 1);
+        c.seed(0, 4);
+
+        c.seed(0, 1);
+        c.seed(2, 3);
+        c.seed(1, 1);
+        c.seed(2, 1);
+        c.seed(3, 1);
+        c.seed(4, 1);
+
+        c.run();
     }
 }
