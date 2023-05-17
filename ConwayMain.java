ae9efd6548b99c747eff8afe3cd2b1fb545c8402
sophialottmann <111463810+sophialottmann@users.noreply.github.com>
2023-05-17 21:19:26 +0000

    itworksnow

--- ConwayMain.java
+++ ConwayMain.java
@@ -2,6 +2,7 @@ public class ConwayMain {
     public static void main(String[] args) {
         Conway c = new Conway(10, 10);
         c.startBoard();
+        c.seed(2, 4);
         c.printBoard();
     }
 }
