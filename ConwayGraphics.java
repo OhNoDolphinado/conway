87e1cde321b0ae04cdea56ac9fc5bfec0bbf2083
apcsa <apcsa@ausdk12.org>
2023-05-24 15:03:10 -0700

    rfyj

--- ConwayGraphics.java
+++ ConwayGraphics.java
@@ -2,56 +2,104 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class ConwayGraphics extends JPanel implements MouseListener{
+// TODO: PaintComponent -- board is stupid (drawRect???)
+// TODO: get it to run and update on the screen 
+// TODO: play and pause -- only change seed when on paused?
+// TODO: fix the size of the screen to reflect the size of the board
 
-    int mouseClickY = -1;
-    int mouseClickX = -1;
+public class ConwayGraphics extends JPanel implements MouseListener, KeyListener {
 
-    int squareSize = 10; // this sets the size of the square (and what everything multiplies by)
+    int mousePositionOnClickX = -1;
+    int mousePositionOnClickY = -1;
+    boolean paused = true;
+    int squareSize = 15; // this sets the size of the square (and what everything multiplies by)
 
     int[][] board;
 
-    ConwayGraphics(int[][] board){
+    ConwayGraphics(int[][] board) {
         this.board = board;
-        JFrame myFrame = new JFrame();      
-        myFrame.setSize(board.length*squareSize, board.length*squareSize); // sets window size to approx. board size
+
+        addMouseListener(this);
+        addKeyListener(this);
+        setFocusable(true);
+        JFrame myFrame = new JFrame();
+
+        myFrame.setSize(500, 500);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.add(this);
         myFrame.setVisible(true);
-      
-        // addMouseListener (this);
+
     }
 
-    public void paintComponent (Graphics gp){ // where do we call this??
+    public void paintComponent(Graphics gp) {
         // read the 2d array board and paint it
+        // boolean test = true;
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[0].length; col++) { // iterate through board values
-                if (board[row][col] == 1){ // determine color to paint board based on value (alive or dead)
-                    gp.setColor(Color.white);
-                } else {
-                    gp.setColor(Color.black);
+                if (board[row][col] == 1) { // determine color to paint board based on value (alive or dead)
+                    gp.setColor(Color.orange);
                 }
-                gp.fillRect(row*squareSize, col*squareSize, (row+1)*squareSize, (col+1)*squareSize);
-                gp.setColor(Color.black);
+                // if (test == true){ // checkerboard testing
+                // gp.setColor(Color.white);
+                // test = false;
+                // } else if (test == false){
+                // gp.setColor(Color.black);
+                // test = true;
+                // }
+
+                gp.fillRect(row * squareSize, col * squareSize, (row + 1) * squareSize, (col + 1) * squareSize);
+                // System.out.println("START: " + row*squareSize + " " + col*squareSize);
+                // System.out.println("END : " + (row+1)*squareSize + " " + (col+1)*squareSize);
+                gp.setColor(Color.blue);
                 // paint the rectangle at the current position (10px by 10px squares)
             }
         }
     }
 
-    public void mouseClicked (MouseEvent event) { // set cell to alive or dead
-        // get x and y from mouse and put it into mouseClick variables
-        mouseClickX = event.getX()*squareSize;
-        mouseClickY = event.getY()*squareSize;
+    public void mouseClicked(MouseEvent event) { // sets the cell to alive or dead on click
+        System.out.println("MOUSE");
+
+        mousePositionOnClickX = Math.floorDiv(event.getX(), squareSize); // get x and y from mouse and put it into mouseClick variables
+        mousePositionOnClickY = Math.floorDiv(event.getY(), squareSize);
+        System.out.println("X: " + mousePositionOnClickX + "Y: " + mousePositionOnClickY);
+
+        if (board[mousePositionOnClickX][mousePositionOnClickY] == 0) { // change the cell by clicking on it
+            board[mousePositionOnClickX][mousePositionOnClickY] = 1;
+        } else {
+            board[mousePositionOnClickX][mousePositionOnClickY] = 0;
+        }
+
         repaint();
     }
-  
-    public void mouseReleased (MouseEvent event) { // nothing
+
+    public void keyPressed(KeyEvent event) { // spacebar for play and pause
+        System.out.println("KEYPRESSED");
+        if (event.getKeyCode() == KeyEvent.VK_SPACE) {
+            paused = !paused;
+            System.out.println("The game is paused? " + paused);
+        }
+        repaint();
     }
-    public void mousePressed (MouseEvent event) { // nothing
+
+    public void mouseReleased(MouseEvent event) { // nothing
     }
-    public void mouseEntered (MouseEvent event) { // nothing
+
+    public void mousePressed(MouseEvent event) { // nothing
     }
-    public void mouseExited (MouseEvent event) { // nothing
+
+    public void mouseEntered(MouseEvent event) { // nothing
     }
+
+    public void mouseExited(MouseEvent event) { // nothing
+    }
+
     public void mouseMoved(MouseEvent event) { // nothing
     }
+
+    public void keyReleased(KeyEvent event) { // nothing
+    }
+
+    public void keyTyped(KeyEvent event) { // nothing
+    }
+
 }
