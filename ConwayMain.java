c65843d144611fff7b3910b50c490f68024699aa
apcsa <apcsa@ausdk12.org>
2023-05-24 15:03:05 -0700

    s;lkjefhdg

--- ConwayMain.java
+++ ConwayMain.java
@@ -1,18 +1,15 @@
 public class ConwayMain {
     public static void main(String[] args) {
-        Conway c = new Conway(5, 5);
+        Conway c = new Conway(15, 5);
         ConwayGraphics cg = new ConwayGraphics(c.getBoard());
         c.startBoard();
-        c.seed(4, 2);
-        c.seed(4, 4);
-        c.seed(3, 4);
-        c.seed(3, 3);
-        c.seed(1, 2);
+        // c.seed(4, 2);
+        // c.seed(4, 4);
+        // c.seed(2, 4);
+        // c.seed(1, 4);
+        // c.seed(1, 2);
         c.printBoard();
-        
-        
-
-        // c.run();
+        c.run();
     }
 
 }
